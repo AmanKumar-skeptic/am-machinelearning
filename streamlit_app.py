ecfb1ade6a8ad9d036310f173ca8179f2a0e25af
Aman Kumar <105311518+AmanKumar-skeptic@users.noreply.github.com>
2024-10-20 01:32:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,9 @@ st.info('This is steel-plate-fault-prediction app')
 
 with st.expander('Data'):
   st.write('**Raw data**')
+
+upload_file = st.file_upload("Upload a text file". type="txt")
+model_choice = st.selectbox("Select a Language model", ["GPT-J", "GPT-NeoX", "LLaMA", "BLOOM"])
+
+if upload_file is not NOne:
+  st.write(f"Uploaded file: {uploaded_file.name}")
